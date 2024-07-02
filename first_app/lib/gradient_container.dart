5fd970896a60e453f8423216602cb61bde666738
schuna <jiyong1972@gmail.com>
2024-07-02 13:48:08 +0900

    section2-42

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -10,9 +10,9 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 20, 1, 37),
             Color.fromARGB(255, 157, 133, 214),
           ],
