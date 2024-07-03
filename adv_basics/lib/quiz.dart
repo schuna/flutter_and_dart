7ab7ef220c16cb24e343429327876bfab2469d7f
schuna <jiyong1972@gmail.com>
2024-07-03 10:00:38 +0900

    section3-63

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -15,17 +15,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -43,7 +37,9 @@ class _QuizState extends State<Quiz> {
         ),
       ),
       child: Center(
-        child: activeScreen,
+        child: activeScreen == 'start-screen'
+            ? StartScreen(switchScreen)
+            : const QuestionScreen(),
       ),
     );
   }
