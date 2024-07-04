6122d2ea59074d7132ea630b8e582fc2d1fb2807
schuna <jiyong1972@gmail.com>
2024-07-04 14:31:38 +0900

    section5-127

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -31,9 +31,9 @@ class MyApp extends StatelessWidget {
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
+                fontWeight: FontWeight.bold,
                 color: kColorScheme.onSecondaryContainer,
-                fontSize: 14
+                fontSize: 16
               ),
             ),
       ),
