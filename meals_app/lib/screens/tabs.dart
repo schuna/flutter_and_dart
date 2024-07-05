7d39bfe19f3d297816e2b39dc8feeac0194819ef
schuna <jiyong1972@gmail.com>
2024-07-05 16:10:35 +0900

    section8-167

--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -22,7 +22,7 @@ class _TabsScreenState extends State<TabsScreen> {
     Widget activePage = const CategoriesScreen();
     var activePageTitle = 'Categories';
     if (_selectedPageIndex == 1) {
-      activePage = const MealsScreen(title: 'Favorites', meals: []);
+      activePage = const MealsScreen(meals: []);
       activePageTitle = 'Your Favorites';
     }
     return Scaffold(
@@ -32,6 +32,7 @@ class _TabsScreenState extends State<TabsScreen> {
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.set_meal),
